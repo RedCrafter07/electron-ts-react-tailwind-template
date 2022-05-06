f014a83e2c2d2b0730cbbefb95f1f07216090ba1
RedCrafter07 <github@redcrafter07.de>
2022-05-06 18:48:33 +0200

    Style stuff with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 
 const App = () => {
-	const [ count, setCount ] = useState(0);
+	const [count, setCount] = useState(0);
 	return (
-		<div>
-			<h1>💖 Hello World!</h1>
-			<p>Welcome to your Electron and React application.</p>
-			<button
-				onClick={() => {
-					setCount(count + 1);
-				}}
-			>
-				{count}
-			</button>
+		<div className='bg-neutral-900 text-white w-screen min-h-screen px-2'>
+			<div className='container mx-auto'>
+				<h1>💖 Hello World!</h1>
+				<p>Welcome to your Electron and React application.</p>
+				<button
+					onClick={() => {
+						setCount(count + 1);
+					}}
+					className='border border-neutral-600 rounded-lg bg-neutral-700 px-2 py-1'
+				>
+					{count}
+				</button>
+			</div>
 		</div>
 	);
 };
